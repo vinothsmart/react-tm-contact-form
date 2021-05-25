af88c92449c739e7b640b4a9db9ea91324e9583f
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-25 15:20:09 +0530

    switching pages with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import { ContactForm, Login } from "./components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <ContactForm />
-      <Login />
+      <Router>
+        {/* <ContactForm />
+        <Login /> */}
+        <Switch>
+          <Route exact path="/" component={ContactForm} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
     </>
   );
 }
